de74cbcaf348679d425f4f66d7ee128cdb987a60
shawee <757221692@qq.com>
2023-05-08 16:32:43 +0800

    problem70

---
+++ src/problem70/Problem70.java
@@ -0,0 +1,29 @@
+package problem70;
+
+import java.util.Scanner;
+
+/**
+ * @Author Shawee
+ * @Date 2023/5/8
+ */
+public class Problem70 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(n));
+    }
+}
+
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+}
