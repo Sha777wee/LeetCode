b5ef7ff5ef0c41854afdb502775148e36a06275c
shawee <757221692@qq.com>
2023-05-08 13:52:51 +0800

    problem112

---
+++ src/problem112/Problem112.java
@@ -0,0 +1,41 @@
+package problem112;
+
+/**
+ * @Author Shawee
+ * @Date 2023/5/8
+ */
+public class Problem112 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
