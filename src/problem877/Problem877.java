498e8b0b4e5edd562424850ad733ab5b4cea6656
shawee <757221692@qq.com>
2023-05-15 22:21:01 +0800

    problem877

---
+++ src/problem877/Problem877.java
@@ -0,0 +1,18 @@
+package problem877;
+
+/**
+ * @Author Shawee
+ * @Date 2023/5/15
+ */
+public class Problem877 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
