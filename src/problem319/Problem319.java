61c89e75bfb42acc51b46ed7ff533c78590a8369
shawee <757221692@qq.com>
2023-05-15 22:58:51 +0800

    problem319

---
+++ src/problem319/Problem319.java
@@ -0,0 +1,17 @@
+package problem319;
+
+/**
+ * @Author Shawee
+ * @Date 2023/5/15
+ */
+public class Problem319 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
